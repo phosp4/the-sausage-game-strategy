1f622d9cb2f44f244f40ac961fb99abe880cacd5
Samuel Dobrík <56543331+phosp4@users.noreply.github.com>
2026-01-14 02:55:56 +0100

    fix SausageWithSwitch logiky

--- the-sausage-game-v2/core/src/main/java/org/example/entities/SausageWithSwitch.java
+++ the-sausage-game-v2/core/src/main/java/org/example/entities/SausageWithSwitch.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 public class SausageWithSwitch extends Sausage {
 
     @Getter @Setter
-    private boolean active = true;
+    private boolean active = false;
 
     public SausageWithSwitch(Player player, Point p1, Point p2, Point p3) {
         super(player, p1, p2, p3);
